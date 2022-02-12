8a2e677ec81783c2458a713273ed6e970f62e0c8
arashi87 <arasi27676271@gmail.com>
2022-02-12 23:47:23 +0800

    refactor: new test framework

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,6 +1,7 @@
 """Testing module"""
 from dataclasses import dataclass
-from typing import Any, Union
+from re import Pattern
+from typing import Union
 
 from app import APP
 from fastapi.testclient import TestClient
@@ -11,54 +12,79 @@ CLIENT = TestClient(APP)
 @dataclass
 class AssertRequest:
     """
-    API request assertion dataclass
-
-    headers (Union[dict, None]): The expected headers of request
-    payload (Union[dict, None]): The expceted payload(json or GET parameters) of request
+    API request assertion class
+    Attributes:
+        method (str): method of request
+        url (str): url of request
+        headers (Union[dict, None]): Headers of request
+        payload (Union[dict, None]): Payload of request (for GET)
     """
 
-    headers: Union[dict, None]
-    payload: Union[dict, None]
+    method: str
+    url: str
+    files: Union[bytes, None] = None
+    headers: Union[dict, None] = None
+    payload: Union[dict, None] = None
 
 
 @dataclass
 class AssertResponse:
     """
-    API response assertion dataclass
-
-    body (Any): The expected body of response
-    status_code (int): The expected status code of response
+    API response assertion class
+    Attributes:
+        body (Union[dict, str]): The expected body of response
+        status_code (int): The expected status code of response
     """
 
-    body: Any = "OK"
-    status_code: int = 200
+    body: Union[dict, str, Pattern]
+    status_code: int
 
 
-def assert_request(
-    method: str, route: str, request: AssertRequest, response: AssertResponse
-):
-    if method.upper() == "GET":
-        resp = CLIENT.request(
-            method,
-            f"{route}",
-            headers=request.headers,
-            params=request.payload,
-        )
-    else:
-        resp = CLIENT.request(
-            method,
-            f"{route}",
-            headers=request.headers,
-            json=request.payload,
-        )
+@dataclass
+class APITestcase:
+    """
+    API testcase assertion class
+    Attributes:
+        name (str): Test case's name
+        request (AssertRequest): Asserted Request
+        response (AssertResponse): Asserted Response
+    """
 
-    try:
+    name: str
+    request: AssertRequest
+    response: AssertResponse
+
+    def run(self):
+        if self.request.files:
+            resp = CLIENT.request(
+                self.request.method,
+                self.request.url,
+                headers=self.request.headers,
+                params=None if self.request.method != "GET" else self.request.payload,
+                data=None if self.request.method == "GET" else self.request.payload,
+                files=self.request.files,
+            )
+        else:
+            resp = CLIENT.request(
+                self.request.method,
+                self.request.url,
+                headers=self.request.headers,
+                params=None if self.request.method != "GET" else self.request.payload,
+                json=None if self.request.method == "GET" else self.request.payload,
+            )
+
+        if isinstance(self.response.body, str):
+            assert (
+                resp.text == self.response.body
+            ), f"{resp.text} does not match {self.response.body}"
+        elif isinstance(self.response.body, Pattern):
+            assert self.response.body.match(
+                resp.text
+            ), f"{resp.text} does not match {self.response.body}"
+        else:
+            assert (
+                resp.json() == self.response.body
+            ), f"{resp.json} does not match {self.response.body}"
         assert (
-            resp.json() == response.body
-        ), f"{resp.json} does not match {response.body}"
-    except Exception:
-        assert resp.text == response.body, f"{resp.body} does not match {response.body}"
-
-    assert (
-        resp.status_code == response.status_code
-    ), f"{resp.status_code} does not match {response.status_code}"
+            resp.status_code == self.response.status_code
+        ), f"{resp.status_code} does not match {self.response.status_code}"
