7bd97ba6a1f9b9f4120791c83b9e31fb6fa5c9d0
arashi87 <arasi27676271@gmail.com>
2022-02-12 23:57:52 +0800

    test: add cleanup function after session end

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,3 +1,5 @@
+import shutil
+
 import pytest
 from db import SESSION
 from models import User
@@ -11,3 +13,13 @@ def preinit():
     """
     SESSION.merge(User(**{"uid": "F74094716", "token": "12345"}))
     SESSION.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup(request):
+    """Cleanup a testing directory once we are finished."""
+
+    def remove_test_dir():
+        shutil.rmtree("data")
+
+    request.addfinalizer(remove_test_dir)
